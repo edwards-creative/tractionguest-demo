518911e2642461a9736346a8eabacef188589c6b
Jon Edwards <jonathan@edwards-creative.com>
2018-07-08 22:09:15 -0700

    Added more content sections and images

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 </section><!-- END ENDORSEMENTS SECTION -->
 
 <!-- START BENEFITS SECTION -->
-    <section id="benefits" class="block accordion">
+    <section id="benefits">
         <!-- START BENEFITS SECTION -->
         <div class="wrapper">
         <article class="benefits__article">
@@ -38,6 +38,7 @@
                     <h4>Completely Portable</h4>
                     <i class="fas fa-wallet"></i>
                     <p>With the same form factor as a credit card you can store HUBBL in your wallet.</p>
+                    <span>▼</span>
                 </li>
             </div>
             <div class="benefits__list--card">
@@ -45,6 +46,7 @@
                     <h4>Lightning Fast Transfers</h4>
                     <i class="fas fa-bolt"></i>
                     <p>Transfer data instantly with our patented light-speed technology.</p>
+                    <span>▼</span>
                 </li>
             </div>
             <div class="benefits__list--card">
@@ -52,6 +54,7 @@
                     <h4>Solar Charged</h4>
                     <i class="fas fa-sun"></i>
                     <p>Harness the power of the sun to charge within minutes.</p>
+                    <span>▼</span>
                 </li>
             </div>
             </ul>
@@ -59,8 +62,56 @@
         </div><!-- SECTION wrapper -->
     </section><!-- END BENEFITS SECTION -->
 
-<section id="pricing">
+<section id="features">
     <!-- START BENEFITS SECTION -->
+    <div class="wrapper">
+    <article class="features__article">
+        <span class="features__article--image portable">
+        </span>
+        <!-- PORTABLE FEATURE -->
+        <div class="features__article--header">
+            <h3 class="article__header--light">Data In Your Pocket</h3>
+            <p>Since HUBBL's the same size as a normal credit card, you'll  be able to take your data with you wherever you go.</p>
+            <div class="features__list--item">
+                <p><i class="fas fa-check"></i>Extremely Portable</p>
+                <p><i class="fas fa-check"></i>Fits in your Pocket</p>
+            </div>
+        </div>
+        <!-- LIGHTNING FAST FEATURE -->
+
+        <div class="features__article--header">
+            <h3 class="article__header--light">Transfer At Light Speed</h3>
+            <p>HUBBL makes it easy to transfer data over the cloud, beyond that, transfers happen almost instantaneously. No more waiting.</p>
+            <div class="features__list--item">
+                <p><i class="fas fa-check"></i>Extremely Portable</p>
+                <p><i class="fas fa-check"></i>Fits in your Pocket</p>
+            </div>
+        </div>
+        <span class="features__article--image light-speed">
+        </span>
+        <!-- SOLAR POWERED -->
+        <span class="features__article--image solar-power">
+        </span>
+        <div class="features__article--header">
+            <h3 class="article__header--light">Transfer At Light Speed</h3>
+            <p>Since HUBBL's the same size as a normal credit card, you'll  be able to take your data with you wherever you go.</p>
+            <div class="features__list--item">
+                <p><i class="fas fa-check"></i>Extremely Portable</p>
+                <p><i class="fas fa-check"></i>Fits in your Pocket</p>
+            </div>
+        </div>
+
+    </article>
+    </div><!-- SECTION wrapper -->
+</section>
+
+
+
+
+
+
+<section id="pricing">
+    <!-- START PRICING SECTION -->
     <div class="wrapper">
 
     </div><!-- SECTION wrapper -->
